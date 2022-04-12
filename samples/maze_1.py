b834048d11cb97f87550aadec3496abf06fbb622
Daxonus <elias@dobrin.net>
2022-04-11 21:46:55 -0700

    added more examples yaya

---
+++ samples/maze_1.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from math import pi
+from random import seed
+from copy import deepcopy
+from multipatprop import System, Transmitter, Receiver, Interferer, Point, DigitalSignal
+from render import render
+
+seed(123)
+
+points_1 = [Point(-2, 0), Point(4, -2), Point(4, 3), Point(-2, 3), Point(-3, 3), Point(-3, 1), Point(2, 2), Point(2, 0), Point(-1, 0), Point(-2, 0)]
+points_2 = [Point(-2, 0), Point(4, -2), Point(4, 3), Point(-2, 3), Point(-3, 3), Point(-3, 1), Point(2, 2), Point(2, 0), Point(-1, 0), Point(-2, 0)]
+
+transmitter = Transmitter(Point(-1.3, -2))
+receiver = Receiver(Point(1.5, 1.5))
+interferers = [Interferer.square(Point(0, 0), 9, 0),
+               Interferer.shape(points=points_1, position=Point(1, 1), scale=0.8, rotation=0),
+               Interferer.shape(points=points_2, position=Point(-1, -1), scale=0.8, rotation=pi)]
+system = System(transmitter, receiver, interferers)
+multipath = system.get_multipath(starting_number=3000, receiver_diameter=0.1, max_reflections=10)
+camera_position = Point(0, 0)
+camera_zoom = 0.1
+render(system, multipath, camera_position, camera_zoom, ui_size=1, bins=15, red_factor=3)
