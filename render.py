7899dab5f16bea6f60a2573347f689e0cba9cce2
Daxonus <elias@dobrin.net>
2022-04-13 14:40:54 -0700

    density visualization working

--- render.py
+++ render.py
@@ -3,7 +3,8 @@ from itertools import pairwise
 import cairo
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.draw import line, line_aa
+from skimage.draw import line
+from skimage.filters import gaussian
 from multipatprop import System, Multipath, Point, Segment
 
 
@@ -67,44 +68,26 @@ def render(system: System, multipath: Multipath, camera_position: Point, camera_
     ax.set_ylabel("Signal Energy")
     ax.set_title("Energy function of propagated waves")
 
-    """
-    camera_minimum = Point(camera_position.x - 0.5 / camera_zoom, camera_position.y - 0.5 / camera_zoom)
-    camera_maximum = Point(camera_position.x + 0.5 / camera_zoom, camera_position.y + 0.5 / camera_zoom)
-    densities = np.zeros((100, 100))
-    for ix, x in enumerate(np.linspace(camera_minimum.x, camera_maximum.x, 100)):
-        for iy, y in enumerate(np.linspace(camera_minimum.y, camera_maximum.y, 100)):
-            density = 0
-            for path in multipath:
-                for point_1, point_2 in pairwise(path):
-                    segment = Segment(point_1, point_2)
-                    density += path.power / Point(x, y).distance(segment)
-            densities[iy][ix] = density
-    densities_flat = densities.flatten()
-    density_low = np.percentile(densities_flat, 5)
-    density_high = np.percentile(densities_flat, 95)
-    fig, ax = plt.subplots()
-    ax.imshow(densities, vmin=density_low, vmax=density_high, origin="lower", cmap="inferno")
-    plt.show()
-    """
-
     camera_minimum = Point(camera_position.x - 0.5 / camera_zoom, camera_position.y - 0.5 / camera_zoom)
     camera_maximum = Point(camera_position.x + 0.5 / camera_zoom, camera_position.y + 0.5 / camera_zoom)
 
     def r_transform(point: Point) -> tuple[int, int]:
-        return (round(49 * (point.y - camera_minimum.y) / (camera_maximum.y - camera_minimum.y)),
-                round(49 * (point.x - camera_minimum.x) / (camera_maximum.x - camera_minimum.x)))
+        return (round((500 - 1) * (point.y - camera_minimum.y) / (camera_maximum.y - camera_minimum.y)),
+                round((500 - 1) * (point.x - camera_minimum.x) / (camera_maximum.x - camera_minimum.x)))
 
-    image = np.zeros((50, 50))
+    image = np.ones((500, 500))
     for path in multipath:
         for point_1, point_2 in pairwise(path):
             r1, c1 = r_transform(point_1)
             r2, c2 = r_transform(point_2)
-            rr, cc, val = line_aa(r1, c1, r2, c2)
-            image[rr, cc] += val
+            rr, cc = line(r1, c1, r2, c2)
+            image[rr, cc] *= 1.1
     image_flat = image.flatten()
-    image_low = np.percentile(image_flat, 10)
-    image_high = np.percentile(image_flat, 90)
+    image_low = np.percentile(image_flat, 5)
+    image_high = np.percentile(image_flat, 95)
+
+    blurred = gaussian(image, sigma=(2, 2), truncate=10)
 
     fig, ax = plt.subplots()
-    ax.imshow(image, vmin=image_low, vmax=image_high, origin="lower", cmap="inferno")
+    ax.imshow(blurred, vmin=image_low, vmax=image_high, origin="lower", cmap="inferno")
     plt.show()
