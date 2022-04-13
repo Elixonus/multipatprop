e871f0ed6687909563c1b8b699322b9159d29537
Daxonus <elias@dobrin.net>
2022-04-12 19:54:54 -0700

    even more city examples

--- samples/city_2.py
+++ samples/city_2.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 from multipatprop import System, Transmitter, Receiver, Interferer, Point, DigitalSignal
 from render import render
 
-seed(1123)
+seed(11230)
 
 
 transmitter = Transmitter(Point(-2, -2.5))
@@ -15,13 +15,15 @@ interferers = [Interferer.square(Point(0, 0), 9, 0)]
 
 for x in range(8):
     for y in range(8):
+        if ((x-4)**2 + (y-4)**2)**0.5 > 4:
+            continue
         interferer = Interferer.rectangle(position=Point(x - 3.5 + random() / 4, y - 3.5 + random() / 4), length=0.3 + random() / 4, width=0.25, rotation=pi / 2 * randint(0, 100))
         interferers.append(interferer)
 
 
 
 system = System(transmitter, receiver, interferers)
-multipath = system.get_multipath(starting_number=1000, receiver_diameter=0.2, max_reflections=40, power_multiplier=0.95)
+multipath = system.get_multipath(starting_number=1000, receiver_diameter=0.2, max_reflections=30, power_multiplier=0.95)
 camera_position = Point(0, 0)
 camera_zoom = 0.1
 render(system, multipath, camera_position, camera_zoom, ui_size=1, bins=30, red_factor=1)
